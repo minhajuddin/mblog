5e7179d698c9f5136cd4185ebe1b514b1d186869
Khaja Minhajuddin <minhajuddin.k@hotmail.com>
2009-04-26 11:25:58 +0530

    Removed a the Microsoft ajax scripts

--- MBlog/src/Data/Domain/Post.cs
+++ MBlog/src/Data/Domain/Post.cs
@@ -6,5 +6,10 @@ namespace MBlog.Data.Domain {
         public string Content { get; set; }
         public string Author { get; set; }
         public DateTime Timestamp { get; set; }
+        public string Gist {
+            get {
+                return Content.Substring(0, 100) + " ....";
+            }
+        }
     }
 }
